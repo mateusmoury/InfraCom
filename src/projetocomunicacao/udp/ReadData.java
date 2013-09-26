159797715379e5caa858362fdab4acadb3c7d055
unknown <mmfrb@G4C32.windows.cin.ufpe.br>
2013-09-26 13:37:31 -0300

    adicionar try

--- src/projetocomunicacao/udp/ReadData.java
+++ src/projetocomunicacao/udp/ReadData.java
@@ -1,85 +1,81 @@
 package projetocomunicacao.udp;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ReadData extends Thread {
-	private HostUDP servidor;
-	private AtomicInteger qntRecebido; 
-        private AtomicBoolean recebeuDados;
-	Object objeto;
 
-	public ReadData(HostUDP servidor) {
-		this.servidor = servidor;
-		this.qntRecebido = new AtomicInteger(0);
-                this.recebeuDados = new AtomicBoolean(false);
-	}
+    private HostUDP servidor;
+    private AtomicInteger qntRecebido;
+    private AtomicBoolean recebeuDados;
+    Object objeto;
 
-	public void run() {
-		boolean loop = true;
-		while (loop) {
-			try {
-				servidor.mutexEnviar.lock();
-				
-				DatagramPacket datagrama = new DatagramPacket(new byte[PacketData.tamanhoPacote], PacketData.tamanhoPacote);
-				try {
-					servidor.socketReceber.receive(datagrama);
-					servidor.ipEnviar = datagrama.getAddress();
-				} catch (IOException e) {
-					servidor.socketReceber.close();
-					this.objeto = Serializer.deserialize(servidor.baos.toByteArray());
-					servidor.recebendo.set(false);
-					break;
-				}
-				
-				if (!ModuloEspecial.descarta()) {
-					PacketData pacote = new PacketData(datagrama.getData(), datagrama.getLength());
-                                        if (pacote.getIsLast() && !recebeuDados.get()) {
-                                            continue;
-                                        }
-					System.out.println(">>>>>> Recebeu pacote num " + pacote.getNumSeq() + " de " + datagrama.getAddress() + " " + datagrama.getPort());
-					if (pacote.getNumSeq() >= servidor.windowBaseRecebe && !servidor.buffer.containsKey(pacote.getNumSeq())) {
-						servidor.buffer.put(pacote.getNumSeq(), pacote);
-						qntRecebido.incrementAndGet();
-					}
-					
-					byte[] ack = (new PacketACK(pacote.getNumSeq())).montaPacote();
-					DatagramPacket dat = new DatagramPacket(ack, ack.length, datagrama.getAddress(), datagrama.getPort());
-					
-					servidor.socketReceber.send(dat);
-                                        
-                                        System.out.println(">>>>>> Envia ack num " + pacote.getNumSeq() + " para " + datagrama.getAddress() + " " + datagrama.getPort());
-					
-					if (pacote.getIsLast()) {
-						for (int i = 0; i < 3; ++i) {
-							Thread.sleep(servidor.packetTimeout);
-							servidor.socketReceber.send(dat);
-						}
-						servidor.socketReceber.close();
-						this.objeto = Serializer.deserialize(servidor.baos.toByteArray());
-						servidor.recebendo.set(false);
-                                                recebeuDados.set(false);
-						break;
-					}
-					
-					PacketData aux;
-					while ((aux = servidor.buffer.get(servidor.windowBaseRecebe)) != null) {
-						servidor.streamReceber.write(aux.getDados());
-						servidor.streamReceber.flush();
-						servidor.windowBaseRecebe++;
-                                                recebeuDados.set(true);
-					}
-					
-					if (qntRecebido.get() == pacote.getTotalPacotes()) {
-						servidor.socketReceber.setSoTimeout(servidor.socketTimeout);
-					}
-				}
-				
-				servidor.mutexEnviar.unlock();
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    public ReadData(HostUDP servidor) {
+        this.servidor = servidor;
+        this.qntRecebido = new AtomicInteger(0);
+        this.recebeuDados = new AtomicBoolean(false);
+    }
+
+    public void run() {
+        boolean loop = true;
+        while (loop) {
+            try {
+                servidor.mutexEnviar.lock();
+                try {
+                    DatagramPacket datagrama = new DatagramPacket(new byte[PacketData.tamanhoPacote], PacketData.tamanhoPacote);
+                    try {
+                        servidor.socketReceber.receive(datagrama);
+                        servidor.ipEnviar = datagrama.getAddress();
+                    } catch (IOException e) {
+                        servidor.socketReceber.close();
+                        this.objeto = Serializer.deserialize(servidor.baos.toByteArray());
+                        servidor.recebendo.set(false);
+                        break;
+                    }
+                    if (!ModuloEspecial.descarta()) {
+                        PacketData pacote = new PacketData(datagrama.getData(), datagrama.getLength());
+                        if (pacote.getIsLast() && !recebeuDados.get()) {
+                            continue;
+                        }
+                        System.out.println(">>>>>> Recebeu pacote num " + pacote.getNumSeq() + " de " + datagrama.getAddress() + " " + datagrama.getPort());
+                        if (pacote.getNumSeq() >= servidor.windowBaseRecebe && !servidor.buffer.containsKey(pacote.getNumSeq())) {
+                            servidor.buffer.put(pacote.getNumSeq(), pacote);
+                            qntRecebido.incrementAndGet();
+                        }
+                        byte[] ack = (new PacketACK(pacote.getNumSeq())).montaPacote();
+                        DatagramPacket dat = new DatagramPacket(ack, ack.length, datagrama.getAddress(), datagrama.getPort());
+                        servidor.socketReceber.send(dat);
+                        System.out.println(">>>>>> Envia ack num " + pacote.getNumSeq() + " para " + datagrama.getAddress() + " " + datagrama.getPort());
+                        if (pacote.getIsLast()) {
+                            for (int i = 0; i < 3; ++i) {
+                                Thread.sleep(servidor.packetTimeout);
+                                servidor.socketReceber.send(dat);
+                            }
+                            servidor.socketReceber.close();
+                            this.objeto = Serializer.deserialize(servidor.baos.toByteArray());
+                            servidor.recebendo.set(false);
+                            recebeuDados.set(false);
+                            break;
+                        }
+                        PacketData aux;
+                        while ((aux = servidor.buffer.get(servidor.windowBaseRecebe)) != null) {
+                            servidor.streamReceber.write(aux.getDados());
+                            servidor.streamReceber.flush();
+                            servidor.windowBaseRecebe++;
+                            recebeuDados.set(true);
+                        }
+                        if (qntRecebido.get() == pacote.getTotalPacotes()) {
+                            servidor.socketReceber.setSoTimeout(servidor.socketTimeout);
+                        }
+                    }
+                } finally {
+                    servidor.mutexEnviar.unlock();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/projetocomunicacao/udp/WriteData.java
+++ src/projetocomunicacao/udp/WriteData.java
@@ -1,82 +1,85 @@
 package projetocomunicacao.udp;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.util.TimerTask;
 
-
 public class WriteData extends Thread {
-	private HostUDP cliente;
 
-	public WriteData(HostUDP cliente) {
-		this.cliente = cliente;
-	}
+    private HostUDP cliente;
 
-	public void run() {
-		while (true) {
-			byte[] data = new byte[PacketData.tamanhoDados];
-			int qntBytesLidos = 0;
-			try {
-				qntBytesLidos = cliente.stream.read(data, 0, PacketData.tamanhoDados);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+    public WriteData(HostUDP cliente) {
+        this.cliente = cliente;
+    }
 
-			if (qntBytesLidos <= 0) {
-				break;
-			}
+    public void run() {
+        while (true) {
+            byte[] data = new byte[PacketData.tamanhoDados];
+            int qntBytesLidos = 0;
+            try {
+                qntBytesLidos = cliente.stream.read(data, 0, PacketData.tamanhoDados);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
-			cliente.mutex.lock();
+            if (qntBytesLidos <= 0) {
+                break;
+            }
 
-			PacketData pacote = new PacketData(data, cliente.proxNumSeq++, false, qntBytesLidos, cliente.qntTotalPacotes);
+            cliente.mutex.lock();
+            try {
+                PacketData pacote = new PacketData(data, cliente.proxNumSeq++, false, qntBytesLidos, cliente.qntTotalPacotes);
+                synchronized (cliente.bufferDado) {
+                    cliente.bufferDado.put(pacote.getNumSeq(), pacote);
+                }
+                if (pacote.getNumSeq() < cliente.windowBase + cliente.windowSize) {
+                    envia(pacote);
+                }
+            } finally {
+                cliente.mutex.unlock();
+            }
+        }
+    }
 
-			synchronized (cliente.bufferDado) {
-				cliente.bufferDado.put(pacote.getNumSeq(), pacote);
-			}
+    public synchronized void envia(PacketData packet) {
+        byte[] pacote = packet.montaPacote();
+        try {
+            System.out.println(">>>>>> Enviando pacote num " + packet.getNumSeq() + " de " + InetAddress.getLocalHost().toString() + " pra " + cliente.ipEnviar + " " + cliente.portaEnviar);
+            cliente.socket.send(new DatagramPacket(pacote, pacote.length, cliente.ipEnviar, cliente.portaEnviar));
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Eu to escutando na porta " + cliente.socket.getLocalPort());
+            cliente.timer.schedule(new Reenvia(packet.getNumSeq(), cliente.ipEnviar, cliente.portaEnviar), cliente.packetTimeout);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-			if (pacote.getNumSeq() < cliente.windowBase + cliente.windowSize) {
-				envia(pacote);
-			}
+    private class Reenvia extends TimerTask {
 
-			cliente.mutex.unlock();
-		}
-	}
+        private int numSeq;
+        InetAddress ip;
+        int pprta;
 
-	public synchronized void envia(PacketData packet) {
-		byte[] pacote = packet.montaPacote();
-		try {
-                    System.out.println(">>>>>> Enviando pacote num " + packet.getNumSeq() + " de " +  InetAddress.getLocalHost().toString() + " pra " + cliente.ipEnviar + " " + cliente.portaEnviar);
-			cliente.socket.send(new DatagramPacket(pacote, pacote.length, cliente.ipEnviar, cliente.portaEnviar));
-                        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> Eu to escutando na porta " + cliente.socket.getLocalPort());
-			cliente.timer.schedule(new Reenvia(packet.getNumSeq(), cliente.ipEnviar, cliente.portaEnviar), cliente.packetTimeout);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        public Reenvia(int numSeq, InetAddress ip, int pprta) {
+            this.numSeq = numSeq;
+            this.ip = ip;
+            this.pprta = pprta;
+        }
 
-	private class Reenvia extends TimerTask {
-		private int numSeq;
-                InetAddress ip;
-                int pprta;
-
-		public Reenvia(int numSeq, InetAddress ip, int pprta) {
-			this.numSeq = numSeq;
-                        this.ip = ip;
-                        this.pprta = pprta;
-		}
-
-		@Override
-		public void run() {
-			cliente.mutex.lock();
-			synchronized (cliente.bufferDado) {
-				PacketData pacote = cliente.bufferDado.get(numSeq);
-				if (pacote != null) {
-                                        System.out.println(">>>>>> Vai reenviar o pacote num " + numSeq + " para " + this.ip + " " + this.pprta);
-					envia(pacote);;
-				}
-			}
-
-			cliente.mutex.unlock();
-		}
-	}
+        @Override
+        public void run() {
+            cliente.mutex.lock();
+            try {
+               // synchronized (cliente.bufferDado) {
+                    PacketData pacote = cliente.bufferDado.get(numSeq);
+                    if (pacote != null) {
+                        System.out.println(">>>>>> Vai reenviar o pacote num " + numSeq + " para " + this.ip + " " + this.pprta);
+                        envia(pacote);
+                    }
+               // }
+            } finally {
+                cliente.mutex.unlock();
+            }
+        }
+    }
 }
